20ebeab3b33f7a1cb441ebfb13838f1016b1be91
robredozav22-svg <robredozav22@gmail.com>
2026-09-08 01:28:22 +0500

    Compile exact Holdem turn chance layer

--- solver-rs/src/lib.rs
+++ solver-rs/src/lib.rs
@@ -16,6 +16,7 @@ pub mod exact_leaf3;
 pub mod exact_range_equity;
 pub mod exact_range_equity3;
 pub mod holdem_river_cfr;
+pub mod holdem_turn_chance;
 pub mod independent_exact_eval;
 pub mod joint;
 pub mod kuhn_cfr;
