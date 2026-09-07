73ef3dec67044dfadb470b2ad3f5dc13952111d8
robredozav22-svg <robredozav22@gmail.com>
2026-09-07 19:14:58 +0500

    Expose strategy comparison diagnostics

--- solver-rs/src/lib.rs
+++ solver-rs/src/lib.rs
@@ -33,6 +33,7 @@ pub mod restricted_exact;
 pub mod restricted_subgame;
 pub mod sb_ev;
 pub mod solver_evidence;
+pub mod strategy_compare;
 pub mod strategy_metrics;
 pub mod strategy_range;
 pub mod strategy_record;
