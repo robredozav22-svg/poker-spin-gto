940f7720bba6d92a41245f21aeea4a0bf2ccb85f
robredozav22-svg <robredozav22@gmail.com>
2026-09-07 16:34:39 +0500

    Expose canonical preflop tree schema

--- solver-rs/src/lib.rs
+++ solver-rs/src/lib.rs
@@ -21,6 +21,7 @@ pub mod payoff_lookup;
 pub mod payoff_manifest;
 pub mod payoff_table;
 pub mod persisted_range_equity;
+pub mod preflop_tree;
 pub mod range;
 pub mod range_equity;
 pub mod range_equity3;
